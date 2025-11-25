51c2479c4ee9ec3d5f173b126cb737ab4a358e3e
kristenn22 <24027209@myrp.edu.sg>
2025-11-25 10:02:36 +0800

    uodate

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const OrderController = require("../controllers/orderController");
 
-router.post("/place-order", OrderController.placeOrder);
 router.get("/invoices", OrderController.showInvoices);
 router.get("/invoice/:id", OrderController.showInvoiceDetails);
 
